14cfae09c3a681f3765ec6291ad38404a7e77fc1
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-09 13:01:34 +0200

    Bugfix: new delimiter %. Removed counter.

--- update_bib.sh
+++ update_bib.sh
@@ -18,16 +18,13 @@ if [ ! -d $SED_BACKUP_DIR ]; then
 fi
 
 
+echo "Replacing bib-file occurrences with ${bib_file} ..."
 ## Replace the current line in which the .bib-file is specified with the new name, for all files in SEARCH_DIR
-counter=0
-
 # Only find files, only find .tex files, ignore any .bak files, ignore files in build directories
 for TEX_FILE in $(find ${SEARCH_DIR} -type f -regex ".*\.tex" ! -regex ".*\.bak" ! -regex ".*/build/.*"); do 
 	if [ -f ${TEX_FILE} ]; then
-		echo "Replacing in ${TEX_FILE}..."
-
-		# Do the actual replacing
-		sed -i".bak" "s/\bibliography{.*_bib}/\bibliography{${bib_file}}/g" ${TEX_FILE} 
+		# Do the actual replacing with % as delimiter (/ gives issues if a bib_file is given with /Users/... for example
+		sed -i".bak" "s%\bibliography{.*_bib}%\bibliography{${bib_file}}%g" ${TEX_FILE}
 	
 		# sed -i"${SED_BACKUP_DIR}/*.bak" ---- doesn't work, so we will move the backup files to the designated folder ourselves
 		# if an identically named file is already in the SED_BACKUP_DIR, we will overwrite it
@@ -35,14 +32,6 @@ for TEX_FILE in $(find ${SEARCH_DIR} -type f -regex ".*\.tex" ! -regex ".*\.bak"
 			rm ${SED_BACKUP_DIR}/"*{TEX_FILE}.bak"
 		fi
 		mv "${TEX_FILE}.bak" ${SED_BACKUP_DIR}
-
-		# Increase the counter
-		let counter+=1
 	fi
 done
-
-if [ counter != 1 ]; then
-	echo "Replaced the bib reference in ${counter} files."
-else
-	echo "Replaced the bib reference in ${counter} file."
-fi
+echo "Done."
