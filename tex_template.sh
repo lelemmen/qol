4f60d5edf28ce85729946f9e7e49c46364ac7062
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-10 15:19:32 +0200

    Fixed some issue when invoking from anywhere else.

--- tex_template.sh
+++ tex_template.sh
@@ -7,20 +7,18 @@
 ### is the (top-level, i.e. all files are put here) directory you would like the project to be created in.
 ### Furthermore, the necessary files to be able to use cmake will be provided in the project_dir as well.
 
-
-
 ## Variable extraction
 project_name=$1
 project_dir=$2
-
+MY_DIR=$(dirname $(readlink ${0}))
 
 ## CMake
 # Copy the cmake dir to project_dir (this directory contains UseLatex.cmake)
 echo "Copying cmake files ..."
-cp -R ./cmake ${project_dir}
+cp -R ${MY_DIR}/cmake ${project_dir}
 
 # Copy and edit (no backup, replace 'project_name' by its actual value) the CMakeLists template
-cp CMakeLists_template.txt ${project_dir}/CMakeLists.txt
+cp ${MY_DIR}/CMakeLists_template.txt ${project_dir}/CMakeLists.txt
 echo "Updating CMakeLists ..."
 sed -i ""  "s/project_name/${project_name}/g" ${project_dir}/CMakeLists.txt
 
@@ -28,4 +26,4 @@ sed -i ""  "s/project_name/${project_name}/g" ${project_dir}/CMakeLists.txt
 ## LaTeX
 # Copy the LaTeX template
 echo "Copying the LaTeX template ..."
-cp template.tex ${project_dir}/${project_name}.tex
+cp ${MY_DIR}/template.tex ${project_dir}/${project_name}.tex
