284178cef70b9d1939a82dae8923bd3ae626726a
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-10 14:18:32 +0200

    First commit of tex_template.sh and its auxiliary files.

---
+++ tex_template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+### This script makes a template LaTeX project.
+###	$1: project_name
+### is the name you want your LaTeX project, so we end up with project_name.tex, which is based on the latex template (template.tex).
+###	$2: project_dir
+### is the (top-level, i.e. all files are put here) directory you would like the project to be created in.
+### Furthermore, the necessary files to be able to use cmake will be provided in the project_dir as well.
+
+
+
+## Variable extraction
+project_name=$1
+project_dir=$2
+
+
+## CMake
+# Copy the cmake dir to project_dir (this directory contains UseLatex.cmake)
+cp -R ./cmake ${project_dir}
+
+# Copy and edit (no backup, replace 'project_name' by its actual value) the CMakeLists template
+cp CMakeLists_template.txt ${project_dir}/CMakeLists.txt
+sed -i"" "s/.*project_name.*/${project_name}/g" ${TEX_FILE}
+
+
+## LaTeX
+# Copy the LaTeX template
+cp tex_template.tex ${project_dir}/${project_name}.tex
