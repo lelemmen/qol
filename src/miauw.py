82c20e1fe684742250a866c33070c769d36c44d0
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-11 23:00:01 +0200

    Special one for Jolien.

---
+++ src/miauw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+print("""                                                                                                                  
+                                                                                                                      
+   SSSSSSSSSSSSSSS                                                                           jjjj                     
+ SS:::::::::::::::S                                                                         j::::j                    
+S:::::SSSSSS::::::S                                                                          jjjj                     
+S:::::S     SSSSSSS                                                                                                   
+S:::::S          nnnn  nnnnnnnn       ooooooooooo       eeeeeeeeeeee    ppppp   ppppppppp  jjjjjjj    eeeeeeeeeeee    
+S:::::S          n:::nn::::::::nn   oo:::::::::::oo   ee::::::::::::ee  p::::ppp:::::::::p j:::::j  ee::::::::::::ee  
+ S::::SSSS       n::::::::::::::nn o:::::::::::::::o e::::::eeeee:::::eep:::::::::::::::::p j::::j e::::::eeeee:::::ee
+  SS::::::SSSSS  nn:::::::::::::::no:::::ooooo:::::oe::::::e     e:::::epp::::::ppppp::::::pj::::je::::::e     e:::::e
+    SSS::::::::SS  n:::::nnnn:::::no::::o     o::::oe:::::::eeeee::::::e p:::::p     p:::::pj::::je:::::::eeeee::::::e
+       SSSSSS::::S n::::n    n::::no::::o     o::::oe:::::::::::::::::e  p:::::p     p:::::pj::::je:::::::::::::::::e 
+            S:::::Sn::::n    n::::no::::o     o::::oe::::::eeeeeeeeeee   p:::::p     p:::::pj::::je::::::eeeeeeeeeee  
+            S:::::Sn::::n    n::::no::::o     o::::oe:::::::e            p:::::p    p::::::pj::::je:::::::e           
+SSSSSSS     S:::::Sn::::n    n::::no:::::ooooo:::::oe::::::::e           p:::::ppppp:::::::pj::::je::::::::e          
+S::::::SSSSSS:::::Sn::::n    n::::no:::::::::::::::o e::::::::eeeeeeee   p::::::::::::::::p j::::j e::::::::eeeeeeee  
+S:::::::::::::::SS n::::n    n::::n oo:::::::::::oo   ee:::::::::::::e   p::::::::::::::pp  j::::j  ee:::::::::::::e  
+ SSSSSSSSSSSSSSS   nnnnnn    nnnnnn   ooooooooooo       eeeeeeeeeeeeee   p::::::pppppppp    j::::j    eeeeeeeeeeeeee  
+                                                                         p:::::p            j::::j                    
+                                                                         p:::::p  jjjj      j::::j                    
+                                                                        p:::::::pj::::jj   j:::::j                    
+                                                                        p:::::::pj::::::jjj::::::j                    
+                                                                        p:::::::p jj::::::::::::j                     
+                                                                        ppppppppp   jjj::::::jjj                      
+                                                                                       jjjjjj """)
+
+
