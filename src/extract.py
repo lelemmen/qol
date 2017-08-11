df09768841df6d8c1f88b0e82b9db7177549d277
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-12 00:28:24 +0200

    Refactored extract (from dotfiles-functions) to python script

---
+++ src/extract.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Extracts any of the following files:
+#   .tar, .tar.gz, .zip
+
+import tarfile
+import zipfile
+
+
+# Parse the argument
+import argparse
+parser = argparse.ArgumentParser(description="Extracts a given file.")
+parser.add_argument('filename', type=str, help="the path to the file you want to extract")
+arguments = parser.parse_args()
+
+filename = arguments.filename
+
+
+print("Trying to extract the given file ...")
+
+if filename.endswith('.tar.gz') or filename.endswith('.tar'):
+    with tarfile.open(filename) as tarf:
+        tarf.extractall()
+elif filename.endswith('.zip'):
+    with zipfile.ZipFile(filename, 'r') as zipf:
+        zipf.extractall()
+else:
+    raise IOError("Cannot extract the given file.")
+
+print("Done.")
