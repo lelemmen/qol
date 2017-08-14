3845c5fb369bf2134f4430ff117a4140acd421c7
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-14 15:59:41 +0200

    Another commit for the missus.

---
+++ src/snoepje.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+
+print("""
+MMMMMMMM               MMMMMMMMIIIIIIIIII               AAA           UUUUUUUU     UUUUUUUUWWWWWWWW                           WWWWWWWW
+M:::::::M             M:::::::MI::::::::I              A:::A          U::::::U     U::::::UW::::::W                           W::::::W
+M::::::::M           M::::::::MI::::::::I             A:::::A         U::::::U     U::::::UW::::::W                           W::::::W
+M:::::::::M         M:::::::::MII::::::II            A:::::::A        UU:::::U     U:::::UUW::::::W                           W::::::W
+M::::::::::M       M::::::::::M  I::::I             A:::::::::A        U:::::U     U:::::U  W:::::W           WWWWW           W:::::W
+M:::::::::::M     M:::::::::::M  I::::I            A:::::A:::::A       U:::::D     D:::::U   W:::::W         W:::::W         W:::::W
+M:::::::M::::M   M::::M:::::::M  I::::I           A:::::A A:::::A      U:::::D     D:::::U    W:::::W       W:::::::W       W:::::W
+M::::::M M::::M M::::M M::::::M  I::::I          A:::::A   A:::::A     U:::::D     D:::::U     W:::::W     W:::::::::W     W:::::W
+M::::::M  M::::M::::M  M::::::M  I::::I         A:::::A     A:::::A    U:::::D     D:::::U      W:::::W   W:::::W:::::W   W:::::W
+M::::::M   M:::::::M   M::::::M  I::::I        A:::::AAAAAAAAA:::::A   U:::::D     D:::::U       W:::::W W:::::W W:::::W W:::::W
+M::::::M    M:::::M    M::::::M  I::::I       A:::::::::::::::::::::A  U:::::D     D:::::U        W:::::W:::::W   W:::::W:::::W
+M::::::M     MMMMM     M::::::M  I::::I      A:::::AAAAAAAAAAAAA:::::A U::::::U   U::::::U         W:::::::::W     W:::::::::W
+M::::::M               M::::::MII::::::II   A:::::A             A:::::AU:::::::UUU:::::::U          W:::::::W       W:::::::W
+M::::::M               M::::::MI::::::::I  A:::::A               A:::::AUU:::::::::::::UU            W:::::W         W:::::W
+M::::::M               M::::::MI::::::::I A:::::A                 A:::::A UU:::::::::UU               W:::W           W:::W
+MMMMMMMM               MMMMMMMMIIIIIIIIIIAAAAAAA                   AAAAAAA  UUUUUUUUU                  WWW             WWW            
+""")
