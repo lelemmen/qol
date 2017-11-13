77a04d0484e49c088ebe3407d54e7f073d6cd9c5
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-11-13 16:49:08 -0500

    Changed Boost_INCLUDE_DIRS to PUBLIC.

--- docs/c++_library_template/cmake/ConfigureTest.cmake
+++ docs/c++_library_template/cmake/ConfigureTest.cmake
@@ -1,7 +1,7 @@
 # In this CMake file, we will include header files and link to libraries for a given test source
 
 # ... add the boost headers (for testing) ...
-target_include_directories(${TEST_NAME} PRIVATE ${Boost_INCLUDE_DIRS})
+target_include_directories(${TEST_NAME} PUBLIC ${Boost_INCLUDE_DIRS})
 
 # ... add this project's headers ...
 target_include_directories(${TEST_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
