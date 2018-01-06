39fcb947c7a8df9323a381065f12501cf1390772
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-01-06 18:23:34 +0100

    Add a script that cleans my home.

---
+++ src/clean_homedoc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Clean my ~/Documents/ folder from League of Legends and Mathematica folders
+
+
+import os
+import shutil
+
+
+home = os.path.expanduser("~")
+league_path = home + "/Documents/League of Legends"
+mathematica_path = home + "/Documents/Wolfram Mathematica"
+
+def remove_if_exists(path_to_dir):
+
+    try:
+        shutil.rmtree(path_to_dir)  # remove a directory recursively
+    except FileNotFoundError:
+        pass
+
+
+
+print("Removing the League of Legends folder ...")
+remove_if_exists(league_path)
+
+print("Removing the Mathematica folder ...")
+remove_if_exists(mathematica_path)
+
+print("Done")
