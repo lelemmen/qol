297874aa8db833c56970ef10a5e85376a7325ca8
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-02-25 17:39:48 -0500

    Een optie 'focus' toegevoegd om elke trainingsdag hetzelfde te doen.

--- src/trainingsprogramma.py
+++ src/trainingsprogramma.py
@@ -10,11 +10,13 @@ parser = argparse.ArgumentParser(description="Stel een trainingsprogramma op met
 parser.add_argument('per_week', metavar='W', type=int, help="het aantal trainingen per week")  # nargs niet specifiëren zodat we geen lijst krijgen
 parser.add_argument('per_dag_functioneel', metavar='F', type=int, help="het aantal functionele oefeningen per trainingsdag")
 parser.add_argument('per_dag_stretches', metavar='S', type=int, help="het aantal stretches per trainingsdag")
+parser.add_argument('focus', metavar='f', type=bool, help="ja/nee om een week lang dezelfde training te doen", nargs='?', default=False)
 arguments = parser.parse_args()
 
 per_dag_functioneel = arguments.per_dag_functioneel
 per_dag_stretches = arguments.per_dag_stretches
 aantal_trainingsdagen = arguments.per_week
+focus = arguments.focus
 
 
 # lijsten met functionele oefeningen en stretches
@@ -28,7 +30,7 @@ stretches = ['1.3',
              '4.7', '4.8', '4.9']
 
 
-# Bouw een trainingsschema op
+# Bouw willekeurig een trainingsschema op
 #   aantal_trainingsdagen keer per_dag_functioneel
 training_functioneel = [random.sample(functionele_oefeningen, per_dag_functioneel) for i in range(0, aantal_trainingsdagen + 1)]
 #   willekeurig voor of na de grote trainingen
@@ -38,6 +40,11 @@ training_stretches = [random.sample(stretches, per_dag_stretches) for i in range
 
 
 # Geef de output van het programma aan de gebruiker
-print("De training van deze week zal zijn:")
-for i in range(aantal_trainingsdagen):
-    print("Dag {}, {} de hoofdtraining: \t {} {}".format(i + 1, voor_of_na_functioneel[i], ' '.join(training_functioneel[i]), ' '.join(training_stretches[i])))
+if focus:
+    print("Je koos voor een week van {} trainingsdagen met de volgende training:".format(aantal_trainingsdagen))
+    print("{} de hoofdtraining: \t {} {}".format(voor_of_na_functioneel[0], ' '.join(training_functioneel[0]), ' '.join(training_stretches[0])))
+else:
+    print("De training van deze week zal zijn:")
+    for i in range(aantal_trainingsdagen):
+        print("Dag {}, {} de hoofdtraining: \t {} {}".format(i + 1, voor_of_na_functioneel[i], ' '.join(training_functioneel[i]), ' '.join(training_stretches[i])))
+
