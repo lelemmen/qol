d3c715750ab0e1f5555c246fcd36b433ccc22853
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-02 07:52:58 -0500

    Now include a wrapper header file in new C++ libraries.

---
+++ docs/c++_library_template/include/wrapper_header.hpp
@@ -0,0 +1,9 @@
+#ifndef &PROJECT_NAME&_&PROJECT_NAME&_HPP
+#define &PROJECT_NAME&_&PROJECT_NAME&_HPP
+
+
+// This file acts as a collective include header
+#include "version.hpp"
+
+
+#endif // &PROJECT_NAME&_&PROJECT_NAME&_HPP
--- src/new.py
+++ src/new.py
@@ -19,7 +19,7 @@ project_type = arguments.type
 project_name = arguments.name
 
 # Since we know what the project's name is going to be, we can figure out the installation path
-project_dir = os.getenv('SOFTWARE_DIR') + '/' + project_name
+project_dir = os.getenv('SOFTWARE_DIR') + '/' + project_name  # project_dir includes the project name
 
 
 # Make sure that the user knows that we only support 'library' for now
@@ -38,14 +38,19 @@ print("Copying files from {} into {} ...".format(qol_dir + '/docs/c++_library_te
 print("Customizing the preset template ...")
 all_template_filenames = glob.glob(qol_dir + '/docs/c++_library_template/**', recursive=True)  # the pattern “**” will match any files and zero or more directories and subdirectories
 
+
 # Filenames starting with a dot are special cases that are not matched, so we'll have to add them manually
 hidden_filenames = glob.glob(qol_dir + '/docs/c++_library_template/.*')
 for hidden_filename in hidden_filenames:
     all_template_filenames.append(hidden_filename)
 
+
 # Copy the contents of every input file into the output file, but replace every occurrence of &PROJECT_NAME&
 for filename in all_template_filenames:
     if '.' not in filename:  # this is a check if we're working with files, not directories
+        continue
+
+    if filename == 'wrapper_header.hpp':  # don't copy-paste the wrapper header template
         continue
 
     # We want to copy all files inside c++_library_template into the project_dir, so we have to figure out what the new filename (inside project_dir)
@@ -53,8 +58,19 @@ for filename in all_template_filenames:
     print(filename_out)
     with open(filename, 'r') as f_in:
         os.makedirs(os.path.dirname(filename_out), exist_ok=True)  # make sure that intermediary directories exist
-        with open(filename_out, 'wt') as f_out:
+        with open(filename_out, 'w') as f_out:
             for line in f_in:
                 f_out.write(line.replace('&PROJECT_NAME&', project_name))
+
+
+# We'll do something special for the wrapper header
+# Create a file with the name 'project_name' in the previously created library include directory
+wrapper_header_filename = project_dir + '/include/' + project_name + '.hpp'
+template_wrapper_header_filename = qol_dir + '/docs/c++_library_template/include/wrapper_header.hpp'
+print(wrapper_header_filename)
+with open(wrapper_header_filename, 'w') as f_out:
+    with open(template_wrapper_header_filename, 'r') as f_in:
+        for line in f_in:
+            f_out.write(line.replace('&PROJECT_NAME&', project_name.upper()))
 
 print("Done.")
