78546791558049020628c76cd407e3d5e1660022
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-05-07 21:05:07 -0400

    Add clrtmp.

---
+++ src/clrtmp.py
@@ -0,0 +1,22 @@
+#!/opt/local/bin/python3
+
+
+import os
+import pathlib
+import shutil
+
+
+
+# Get the personal temporary directory from the environment
+tmp_directory_path = os.environ["PERS_TEMP_DIR"]
+cpp_tmp_dir = os.path.join(tmp_directory_path, "C++")
+python_tmp_dir = os.path.join(tmp_directory_path, "Python")
+
+
+# Remove everything that's in it
+shutil.rmtree(tmp_directory_path)
+
+
+# Remake the Python and C++ directories
+pathlib.Path(cpp_tmp_dir).mkdir(parents=True)
+pathlib.Path(python_tmp_dir).mkdir(parents=True)
